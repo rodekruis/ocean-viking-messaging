a961ba29e4707b5014e9432518448183d0e4e652
jmargutti <jacopo.margutti@gmail.com>
2022-05-02 14:48:25 +0200

    remove dependency on azure blob storage

--- pipeline/src/pipeline/pipeline.py
+++ pipeline/src/pipeline/pipeline.py
@@ -2,11 +2,12 @@ import datetime
 import requests
 import pandas as pd
 import os
+import time
 from twilio.rest import Client
 from dotenv import load_dotenv
-from azure.storage.blob import BlobServiceClient
 from twilio.base.exceptions import TwilioRestException
 import click
+import json
 import logging
 logging.root.handlers = []
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG, filename='ex.log')
@@ -19,26 +20,11 @@ console.setFormatter(formatter)
 logging.getLogger("").addHandler(console)
 twilio_logger = logging.getLogger('twilio.http_client')
 twilio_logger.setLevel(logging.WARNING)
-load_dotenv(dotenv_path="../credentials/.env")
-
-
-def get_blob_service_client(blob_path):
-    blob_service_client = BlobServiceClient.from_connection_string(os.getenv("CONNECTION"))
-    return blob_service_client.get_blob_client(container='messaging', blob=blob_path)
-
-
-def upload_data(data_path='messages.csv', blob_path='messages.csv'):
-    # upload data to azure blob storage
-    blob_client = get_blob_service_client(blob_path)
-    with open(data_path, "rb") as data:
-        blob_client.upload_blob(data, overwrite=True)
-
-
-def download_data(data_path='messages.csv', blob_path='messages.csv'):
-    # download data from azure blob storage
-    blob_client = get_blob_service_client(blob_path)
-    with open(data_path, "wb") as download_file:
-        download_file.write(blob_client.download_blob().readall())
+# load credentials
+load_dotenv(dotenv_path="credentials/.env")
+# set valid message statuses
+VALID_MESSAGE_STATUS = ['delivered',
+                        'not_delivered']
 
 
 def get_kobo_data():
@@ -51,9 +37,31 @@ def get_kobo_data():
     if 'results' in data.keys():
         df_form = pd.DataFrame(data['results'])
     else:
-        df_form = pd.DataFrame(columns=['rescue_number', 'gender', 'age', 'accompanied',
-                                        'accompanied_by_who', 'pregnant', 'country'])
+        df_form = pd.DataFrame(columns=['name', 'language', 'telephone', 'message_status'])
     return df_form
+
+
+def update_kobo_data(submission_id, status):
+    """update message_status of one submission"""
+    if status not in VALID_MESSAGE_STATUS:
+        raise ValueError("update_kobo_data: status must be one of %r." % VALID_MESSAGE_STATUS)
+
+    # update submission in kobo
+    url = f'https://kobonew.ifrc.org/api/v2/assets/{os.getenv("ASSET")}/data/bulk/'
+    headers = {'Authorization': f'Token {os.getenv("TOKEN")}'}
+    params = {'format': 'json'}
+    payload = {
+        "submission_ids": [str(submission_id)],
+        "data": {'message_status': status}
+    }
+    print(payload)
+    r = requests.patch(
+        url=url,
+        data={'payload': json.dumps(payload)},
+        params=params,
+        headers=headers
+    )
+    return r
 
 
 @click.command()
@@ -61,50 +69,75 @@ def main():
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
-    df = get_kobo_data()
-
-    download_data('messages-sent.csv', 'messages-sent.csv')
-    df_historical = pd.read_csv('messages-sent.csv')
-
-    df_to_send = df[~df['_uuid'].isin(df_historical['_uuid'])]
+    df_to_send = get_kobo_data()
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
-
     df_message_text = pd.read_excel('../data/message-text.xlsx')
 
     for ix, row in df_to_send.iterrows():
+        if row['message_status'] == 'new':
+            name = row['name']
+            lang = row['language']
+            phone = row['telephone']
 
-        name = row['name']
-        lang = row['language']
-        phone = row['telephone']
+            message_text = df_message_text.loc[df_message_text['language'] == lang]['message'].values[0]
+            message_text = message_text.replace('123', name)
+            from_text = df_message_text.loc[df_message_text['language'] == lang]['from'].values[0]
+            is_sender_alphanumeric = True
+            sid = 0
+            delivered = False
 
-        message_text = df_message_text.loc[df_message_text['language'] == lang]['message'].values[0]
-        message_text = message_text.replace('123', name)
-        from_text = df_message_text.loc[df_message_text['language'] == lang]['from'].values[0]
-
-        try:
-            message = client.messages.create(
-                body=str(message_text),
-                from_=str(from_text),
-                to='+'+str(phone)
-            )
-            df_to_send.at[ix, "_notes"] = "submitted as OV"
-        except TwilioRestException as e:
             try:
                 message = client.messages.create(
                     body=str(message_text),
-                    from_='+'+os.environ['TWILIO_PHONE_NUMBER'],
-                    to='+'+str(phone)
+                    from_=str(from_text),
+                    to='+' + str(phone)
                 )
-                df_to_send.at[ix, "_notes"] = "submitted as number"
+                sid = message.sid
             except TwilioRestException as e:
                 logging.error(e)
-                df_to_send.at[ix, "_notes"] = f"error: {e}"
+                is_sender_alphanumeric = False
+                try:
+                    message = client.messages.create(
+                        body=str(message_text),
+                        from_='+' + os.environ['TWILIO_PHONE_NUMBER'],
+                        to='+' + str(phone)
+                    )
+                    sid = message.sid
+                except TwilioRestException as e:
+                    logging.error(e)
 
-    df_historical = df_historical.append(df_to_send, ignore_index=True)
-    df_historical.to_csv('messages-sent.csv', index=False)
-    upload_data('messages-sent.csv', 'messages-sent.csv')
+            # wait 30 seconds and check message status;
+            time.sleep(30)
+            try:
+                message = client.messages(sid).fetch()
+                error_code = message.error_code
+                if error_code is None:
+                    delivered = True
+                # if failed with error 30008 (unknown error), try without alphanumeric sender
+                if error_code == 30008 and is_sender_alphanumeric:
+                    try:
+                        client.messages.create(
+                            body=str(message_text),
+                            from_='+' + os.environ['TWILIO_PHONE_NUMBER'],
+                            to='+' + str(phone)
+                        )
+                        time.sleep(30)
+                        message = client.messages(sid).fetch()
+                        error_code = message.error_code
+                        if error_code is None:
+                            delivered = True
+                    except TwilioRestException as e:
+                        logging.error(e)
+            except TwilioRestException as e:
+                logging.error(e)
+
+            submission_id = row['_id']
+            if delivered:
+                update_kobo_data(submission_id, 'delivered')
+            else:
+                update_kobo_data(submission_id, 'not_delivered')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
